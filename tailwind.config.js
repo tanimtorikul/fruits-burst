9977b650a35d2276599f8b62437de769c00ae653
torikulswe <torikulswe@gmail.com>
2023-07-26 13:37:54 +0600

    team section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,3 +6,16 @@ module.exports = {
     },
     plugins: [],
   }
+  module.exports = {
+    theme: {
+      container: {
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '4rem',
+          xl: '5rem',
+          '2xl': '6rem',
+        },
+      },
+    },
+  };
